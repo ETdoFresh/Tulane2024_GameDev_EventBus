df9f493882412d9f3b6a92f088f6db792f61baf7
ETdoFresh <ETdoFresh@gmail.com>
2024-06-30 21:54:31 -0500

    Add coin example

---
+++ EventBusExample/Assets/1_UnityPersistentEvents/CoinTextBehaviour.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _1_UnityPersistentEvents
+{
+    public class CoinTextBehaviour : MonoBehaviour
+    {
+        public UnityEvent onCoinCollected;
+        [SerializeField] private TMP_Text coinText;
+
+        private void OnEnable()
+        {
+            onCoinCollected.AddListener(OnCoinCollected);
+        }
+        
+        private void OnDisable()
+        {
+            onCoinCollected.RemoveListener(OnCoinCollected);
+        }
+        
+        private void OnCoinCollected()
+        {
+            coinText.color = Color.yellow;
+        }
+    }
+}
